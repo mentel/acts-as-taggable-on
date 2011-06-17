c3a0ea23897f563b1d99d98f3109edaa4e70f1b8
Guillaume Malette <gmalette@mentel.com>
2011-06-17 13:51:12 -0400

    Make Tags queries i18n

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -6,16 +6,16 @@ module ActsAsTaggableOn
     
     translates :name
     
-    default_scope joins(ActsAsTaggableOn::Tag::translations_table_name)
-
+    before_create :set_token
+    
     ### ASSOCIATIONS:
 
     has_many :taggings, :dependent => :destroy, :class_name => 'ActsAsTaggableOn::Tagging'
 
     ### VALIDATIONS:
 
-    validates_presence_of :token
-    validates_uniqueness_of :token
+    #validates_presence_of :token
+    #validates_uniqueness_of :token
 
     ### SCOPES:
     
@@ -24,24 +24,25 @@ module ActsAsTaggableOn
     end
 
     def self.named(name)
-      where(["#{self.translations_table_name}.name #{like_operator} ?", name])
+      where(["#{self.translations_table_name}.name #{like_operator} ?", name]).includes(:translations)
     end
   
     def self.named_any(list)
-      where(list.map { |tag| sanitize_sql(["#{self.translations_table_name}.name #{like_operator} ?", tag.to_s]) }.join(" OR "))
+      includes(:translations).where(list.map { |tag| sanitize_sql(["#{self.translations_table_name}.name #{like_operator} ?", tag.to_s]) }.join(" OR "))
     end
   
     def self.named_like(name)
-      where(["#{self.translations_table_name}.name #{like_operator} ?", "%#{name}%"])
+      where(["#{self.translations_table_name}.name #{like_operator} ?", "%#{name}%"]).includes(:translations)
     end
 
     def self.named_like_any(list)
-      where(list.map { |tag| sanitize_sql(["#{self.translations_table_name}.name #{like_operator} ?", "%#{tag.to_s}%"]) }.join(" OR "))
+      includes(:translations).where(list.map { |tag| sanitize_sql(["#{self.translations_table_name}.name #{like_operator} ?", "%#{tag.to_s}%"]) }.join(" OR "))
     end
 
     ### CLASS METHODS:
 
     def self.find_or_create_with_like_by_name(name)
+      raise named_like(name).inspect
       named_like(name).first || create(:name => name)
     end
 
@@ -49,11 +50,13 @@ module ActsAsTaggableOn
       list = [list].flatten
 
       return [] if list.empty?
-
+      
       existing_tags = Tag.named_any(list).all
+      
+      #raise existing_tags.collect{|t| comparable_name(t.name)}.inspect + list.collect{|n| comparable_name(n)}.inspect unless existing_tags.empty?
+      
       new_tag_names = list.reject do |name| 
-                        name = comparable_name(name)
-                        existing_tags.any? { |tag| comparable_name(tag.name) == name }
+                        existing_tags.any? { |tag| comparable_name(tag.name) == comparable_name(name) }
                       end
       created_tags  = new_tag_names.map { |name| Tag.create(:name => name) }
 
@@ -72,6 +75,10 @@ module ActsAsTaggableOn
 
     def count
       read_attribute(:count).to_i
+    end
+    
+    def set_token
+      token = ""#name.downcase if (token.nil? || token.empty?) && !name.nil?
     end
 
     class << self
